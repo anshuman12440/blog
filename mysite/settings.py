7e40f178656a439609f10985002224ebf2fc2097
Anshuman kumar <anshumankumar.899@gmail.com>
2022-03-11 13:34:29 +0530

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s2j-o5bk*%_e*j0rsu+ri(^@ymvu85_1ea51l@d&&mq3p#)cay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','bcvn.herokuapp.com/']
 
  
 # Application definition
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root'}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root'}), 
 ]
+
 
     
